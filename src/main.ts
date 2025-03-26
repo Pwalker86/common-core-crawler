3f34a3fb5c38423ddb4a55be2bd810d2f2dde5df
Phil Walker <phil1586@gmail.com>
2025-03-25 23:53:01 -0600

    scrape common core standards for math and save

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 // For more information, see https://crawlee.dev/
-import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
+import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 
-import { router } from './routes.js';
+import { router } from "./routes.js";
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ["https://www.education.com/common-core/"];
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  // Comment this option to scrape the full website.
+  maxRequestsPerCrawl: 20,
 });
 
 await crawler.run(startUrls);
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,35 @@
-import { createCheerioRouter } from 'crawlee';
+import { createCheerioRouter } from "crawlee";
 
 export const router = createCheerioRouter();
 
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
+  log.info(`enqueueing new URLs`);
+  await enqueueLinks({
+    globs: ["https://www.education.com/common-core/**/math/"],
+    label: "detail",
+  });
 });
 
-router.addHandler('detail', async ({ request, $, log, pushData }) => {
-    const title = $('title').text();
-    log.info(`${title}`, { url: request.loadedUrl });
+router.addHandler("detail", async ({ request, $, log, pushData }) => {
+  const linkDivs = $(".ccss-node-name").toArray();
+  const linkDescriptions = $(".ccss-node-description").toArray();
 
-    await pushData({
-        url: request.loadedUrl,
-        title,
+  log.info(`found ${linkDivs.length} links`, { url: request.loadedUrl });
+
+  const data = linkDivs.reduce((acc, el, index) => {
+    const link = $(el).text();
+    const description = $(linkDescriptions[index]).text();
+
+    log.info(`found link: ${link} with description: ${description}`, {
+      url: request.loadedUrl,
     });
+
+    acc[link.trim()] = description;
+    return acc;
+  }, {});
+
+  await pushData({
+    url: request.loadedUrl,
+    data,
+  });
 });
